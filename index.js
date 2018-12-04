faeb765507e9f592180582a31a75a1a472272386
Captainfive <vinou_montes@hotmail.fr>
2018-12-04 15:49:19 +0100

    refactor: removing readfile and exec db

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
+// Require NodeJS Dependencies
 const polka = require("polka");
 const sqlite = require("sqlite");
 const { readFile } = require("fs").promises;
 
-async function main() {
-    console.log("Connect SQLite database");
+/**
+ * @async
+ * @function serverPolka
+ * @returns {void}
+*/
+async function serverPolka() {
+    // Create Local Database Constante
     const db = await sqlite.open("./database.sqlite");
-
-    const sql = await readFile("./mytable.sql", { encoding: "utf8" });
-    await db.exec(sql);
-
-    console.log("done");
 
     // Create HTTP Server
     const server = polka();
 
+    // Global middleware
     server.use((req, res, next) => {
         res.json = function json(payload) {
             return res.end(JSON.stringify(payload));
-        }
+        };
 
         next();
     });
 
-    server.get("/", async (req, res) => {
+    // Uptime endpoint
+    server.get("/", async(req, res) => {
         res.json({
             uptime: process.uptime()
         });
     });
 
-    server.get("/addons", async (req, res) => {
+    // Addons endpoint
+    server.get("/addons", async(req, res) => {
         const addons = await db.all("SELECT DISTINCT name FROM addons");
 
         res.json(addons.map((row) => row.name));
     });
 
-    server.get("/addons/:addonName", async (req, res) => {
+    // Addon Name endpoint
+    server.get("/addons/:addonName", async(req, res) => {
         const addonName = req.params.addonName;
 
-        const addon = await db.get(`SELECT DISTINCT name, description FROM addons WHERE name=?`, addonName.slice(0));
+        const addon = await db.get("SELECT DISTINCT name, description FROM addons WHERE name=?", addonName.slice(0));
         if (typeof addon === "undefined") {
             return res.json({ error: "Addon not found!" });
         }
@@ -45,8 +50,10 @@ async function main() {
         return res.json(addon);
     });
 
+    // Boot Http Server
     server.listen(1337, () => {
         console.log("HTTP Server started on port 1337");
     });
 }
-main().catch(console.error);
+serverPolka().catch(console.error);
+// catch errors
