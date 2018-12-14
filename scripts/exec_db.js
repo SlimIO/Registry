77b7ea2c2874a049e8a306d12b63046b7897e49e
Captainfive <vinou_montes@hotmail.fr>
2018-12-14 17:36:50 +0100

    fix(duplication): database inserts

--- scripts/exec_db.js
+++ scripts/exec_db.js
@@ -1,14 +1,16 @@
-// Require NodeJS Dependencies
-const sqlite = require("sqlite");
+// Require Node.js Dependencies
 const { readFile } = require("fs").promises;
+
+// Require Third-party Dependencies
+const sqlite = require("sqlite");
 
 /**
  * @async
  * @function readData
  * @returns {Promise<void>}
- *  */
+ */
 async function readData() {
     const db = await sqlite.open("../database.sqlite");
-    const sql = await readFile("../registry.sql", { encoding: "utf8" });
+    const sql = await readFile("./registry.sql", { encoding: "utf8" });
     await db.exec(sql);
 } readData().catch(console.error);
