dd783908894e242ea3eef7455e0d3c72078e88e7
Captainfive <vinou_montes@hotmail.fr>
2018-12-20 18:12:13 +0100

    chore: adding tables to registry.sql

--- scripts/registry.sql
+++ scripts/registry.sql
@@ -1,3 +1,31 @@
+-- adding users
+CREATE TABLE IF NOT EXISTS "users" (
+    "id" INTEGER PRIMARY KEY NOT NULL,
+    "username" VARCHAR(40) NOT NULL,
+    "password" VARCHAR(240) NOT NULL
+);
+
+-- adding organisations
+CREATE TABLE IF NOT EXISTS "orga" (
+    "id" INTEGER PRIMARY KEY NOT NULL,
+    "name" VARCHAR(25) NOT NULL
+);
+
+-- distribution of rights
+CREATE TABLE IF NOT EXISTS "slimio_user" (
+    "user_id" INTEGER NOT NULL,
+    "orga_id" INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "private_user" (
+    "user_id" INTEGER NOT NULL,
+    "orga_id" INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "public_user" (
+    "user_id" INTEGER NOT NULL,
+    "orga_id" INTEGER NOT NULL
+);
+
+-- adding addons
 CREATE TABLE IF NOT EXISTS "addons" (
     "id" INTEGER PRIMARY KEY NOT NULL,
     "name" VARCHAR(35),
@@ -5,11 +33,4 @@ CREATE TABLE IF NOT EXISTS "addons" (
     "version" VARCHAR(20),
     "author" VARCHAR(25),
     "git" VARCHAR(120)
-);
-
-CREATE TABLE IF NOT EXISTS "users" (
-    "id" INTEGER PRIMARY KEY NOT NULL,
-    "username" VARCHAR(40) NOT NULL,
-    "password" VARCHAR(240) NOT NULL,
-    "admin" BOOLEAN INTEGER NOT NULL
 );
