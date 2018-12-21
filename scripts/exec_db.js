0c5adde8168d18d6a462dae5abdb20dbf9549249
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-12-21 13:16:04 +0100

    fix: add __dirname path & fix typo

--- scripts/exec_db.js
+++ scripts/exec_db.js
@@ -1,5 +1,6 @@
 // Require Node.js Dependencies
 const { readFile } = require("fs").promises;
+const { join } = require("path");
 
 // Require Third-party Dependencies
 const sqlite = require("sqlite");
@@ -11,6 +12,8 @@ const sqlite = require("sqlite");
  */
 async function readData() {
     const db = await sqlite.open("../database.sqlite");
-    const sql = await readFile("./registry.sql", { encoding: "utf8" });
+    const sql = await readFile(join(__dirname, "registry.sql"), { encoding: "utf8" });
+
     await db.exec(sql);
-} readData().catch(console.error);
+}
+readData().catch(console.error);
