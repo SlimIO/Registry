e8b69277b2d691e67faaba8bb7900566325f1c71
Captainfive <vinou_montes@hotmail.fr>
2018-12-25 11:15:42 +0100

    test(prototype): adding test for addons endpoint

--- test/test.js
+++ test/test.js
@@ -34,3 +34,11 @@ ava("GET / must return uptime", async(assert) => {
     assert.true(is.number(body.uptime));
     assert.deepEqual(Object.keys(body), ["uptime"]);
 });
+
+ava("GET /addons must return an array", async(assert) => {
+    const { body } = await got(HTTP_URL, { json: true });
+
+    assert.true(is.array(body, "body is an array"));
+    assert.true(Reflect.has(body, "addons"));
+    assert.deepEqual(Object.keys(body), ["addons"]);
+});
