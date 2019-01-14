a2e1cb91f2721c7bad228b13d25f91cec0a3f808
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-01-14 12:33:36 +0100

    chore: remove org table

--- scripts/registry.sql
+++ scripts/registry.sql
@@ -5,26 +5,6 @@ CREATE TABLE IF NOT EXISTS "users" (
     "password" VARCHAR(240) NOT NULL
 );
 
--- adding organisations
-CREATE TABLE IF NOT EXISTS "orga" (
-    "id" INTEGER PRIMARY KEY NOT NULL,
-    "name" VARCHAR(25) NOT NULL
-);
-
--- distribution of rights
-CREATE TABLE IF NOT EXISTS "slimio_user" (
-    "user_id" INTEGER NOT NULL,
-    "orga_id" INTEGER NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "private_user" (
-    "user_id" INTEGER NOT NULL,
-    "orga_id" INTEGER NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "public_user" (
-    "user_id" INTEGER NOT NULL,
-    "orga_id" INTEGER NOT NULL
-);
-
 -- adding addons
 CREATE TABLE IF NOT EXISTS "addons" (
     "id" INTEGER PRIMARY KEY NOT NULL,
