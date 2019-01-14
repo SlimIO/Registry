637e375313488f97c361f3110cce009ab96a256f
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-01-14 12:47:21 +0100

    refactor: improve main http server

--- src/httpServer.js
+++ src/httpServer.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+require("make-promises-safe");
+
 // Require Third-party Dependencies
 const polka = require("polka");
 const bodyParser = require("body-parser");
@@ -5,7 +8,9 @@ const jwt = require("jsonwebtoken");
 const send = require("@polka/send-type");
 const argon2 = require("argon2");
 const indicative = require("indicative");
-require("dotenv").config();
+
+// Require Internal Dependencies
+const { loginForm, addonForm } = require("./formatRules");
 
 // CONSTANTS
 const SECRET_KEY = process.env.registry_secret || "default_secret";
@@ -22,29 +27,18 @@ function isAuthenticated(req, res, next) {
 
 // Create POLKA Server
 const server = polka();
-
-// Global middleware
 server.use(bodyParser.json());
-server.use((req, res, next) => {
-    res.json = function json(payload) {
-        return res.end(JSON.stringify(payload));
-    };
-
-    next();
-});
 
 // Uptime endpoint
 server.get("/", async(req, res) => {
-    res.json({
-        uptime: process.uptime()
-    });
+    send(res, 200, { uptime: process.uptime() });
 });
 
 // Addons endpoint
 server.get("/addons", async(req, res) => {
     const addons = await req.db.all("SELECT DISTINCT name FROM addons");
 
-    res.json(addons.map((row) => row.name));
+    send(res, 200, addons.map((row) => row.name));
 });
 
 // Addon Name endpoint
@@ -53,18 +47,16 @@ server.get("/addons/:addonName", async(req, res) => {
 
     const addon = await req.db.get("SELECT DISTINCT name, description FROM addons WHERE name=?", addonName.slice(0));
     if (typeof addon === "undefined") {
-        return res.json({ error: "Addon not found!" });
+        return send(res, 503, { error: "Addon not found!" });
     }
 
-    return res.json(addon);
+    return send(res, 200, addon);
 });
 
 // Login endpoint
 server.post("/login", async(req, res) => {
+    await indicative.validateAll(req.body, loginForm);
     const { username, password } = req.body;
-    if (!username) {
-        return send(res, 400, "You need a login and a password");
-    }
 
     const user = await req.db.get(
         "SELECT DISTINCT username, password, id FROM users WHERE username=? AND password=?",
@@ -81,25 +73,18 @@ server.post("/login", async(req, res) => {
         return send(res, 401, "Invalid User Password");
     }
 
-    const token = jwt.sign({
+    // eslint-disable-next-line
+    const access_token = jwt.sign({
         sub: user.id,
         username: user.username
     }, SECRET_KEY, { expiresIn: "3 hours" });
 
-    return send(res, 200, { access_token: token });
+    return send(res, 200, { access_token });
 });
-
-const AddonRules = {
-    name: "required|string|min:2",
-    description: "required|string",
-    version: "required|string",
-    author: "required|string",
-    git: "required|string"
-};
 
 // slimio post addon
 server.post("/publishAddon", isAuthenticated, async(req, res) => {
-    await indicative.validateAll(req.body, AddonRules);
+    await indicative.validateAll(req.body, addonForm);
     const { name, description, version, author, git } = req.body;
 
     const { lastID } = await req.db.run("INSERT INTO addons (name, description, version, author, git) VALUES (?, ?, ?, ?, ?)",
