467b69619d5cf26bafa83e28e342cfc4b0cffcc2
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-03-21 19:21:42 +0100

    refactor: comment GET methods

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,28 +7,6 @@ const argon2 = require("argon2");
 // Create router
 const server = polka();
 
-// all users endpoint
-server.get("/", async(req, res) => {
-    const users = await req.Users.findAll({
-        attributes: { exclude: ["password", "id"] },
-        include: [{
-            model: req.Addons,
-            attributes: ["name", "description"],
-            include: {
-                model: req.Organisation,
-                attributes: ["name"]
-            }
-        },
-        {
-            model: req.Organisation,
-            attributes: ["name", "description"],
-            through: { attributes: [] }
-        }]
-    });
-
-    return send(res, 200, users);
-});
-
 // create user endpoint
 server.post("/", async(req, res) => {
     const { username, password } = req.body;
@@ -55,35 +33,57 @@ server.post("/", async(req, res) => {
     return send(res, 200, { userId: user.id });
 });
 
+// all users endpoint
+// server.get("/", async(req, res) => {
+//     const users = await req.Users.findAll({
+//         attributes: { exclude: ["password", "id"] },
+//         include: [{
+//             model: req.Addons,
+//             attributes: ["name", "description"],
+//             include: {
+//                 model: req.Organisation,
+//                 attributes: ["name"]
+//             }
+//         },
+//         {
+//             model: req.Organisation,
+//             attributes: ["name", "description"],
+//             through: { attributes: [] }
+//         }]
+//     });
+
+//     return send(res, 200, users);
+// });
+
 // user Name endpoint
-server.get("/:userName", async(req, res) => {
-    const userName = req.params.userName;
+// server.get("/:userName", async(req, res) => {
+//     const userName = req.params.userName;
 
-    const user = await req.Users.findOne({
-        where: { userName },
-        attributes: { exclude: ["password", "id"] },
-        include: [
-            {
-                model: req.Addons,
-                attributes: ["name", "description"],
-                include: {
-                    model: req.Organisation,
-                    attributes: ["name"]
-                }
-            },
-            {
-                model: req.Organisation,
-                attributes: ["name", "description"],
-                through: { attributes: [] }
-            }
-        ]
-    });
+//     const user = await req.Users.findOne({
+//         where: { userName },
+//         attributes: { exclude: ["password", "id"] },
+//         include: [
+//             {
+//                 model: req.Addons,
+//                 attributes: ["name", "description"],
+//                 include: {
+//                     model: req.Organisation,
+//                     attributes: ["name"]
+//                 }
+//             },
+//             {
+//                 model: req.Organisation,
+//                 attributes: ["name", "description"],
+//                 through: { attributes: [] }
+//             }
+//         ]
+//     });
 
-    if (is.nullOrUndefined(user)) {
-        return send(res, 500, { error: "User not found!" });
-    }
+//     if (is.nullOrUndefined(user)) {
+//         return send(res, 500, { error: "User not found!" });
+//     }
 
-    return send(res, 200, user);
-});
+//     return send(res, 200, user);
+// });
 
 module.exports = server;
