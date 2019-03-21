462255b2dec86451c743e5224f726742734b6425
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-03-21 18:45:38 +0100

    chore: cleanup index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 require("make-promises-safe");
-
-// Require Third-party Dependencies
-const sqlite = require("sqlite");
-const { yellow } = require("kleur");
 require("dotenv").config();
 
-// Require Internal Dependencies
+// Require Third-party Dependencies
+const { yellow } = require("kleur");
 const Sequelize = require("sequelize");
+
+// Require Internal Dependencies
 const server = require("./src/httpServer");
 const models = require("./src/models");
 
@@ -20,12 +19,7 @@ const PORT = process.env.PORT || 1337;
  */
 async function main() {
     console.log(` > open SQLite database: ${yellow("./database.sqlite")}`);
-    // sequelize = new Sequelize("database.db", "username", null, {
-    //     storage: "database.db",
-    //     dialect: "sqlite",
-    //     logging: false
-    // });
-    sequelize = new Sequelize("test", "root", "root", { dialect: "mysql", logging: false });
+    const sequelize = new Sequelize("test", "root", "root", { dialect: "mysql", logging: false });
     const tables = models(sequelize);
 
     await sequelize.sync();
