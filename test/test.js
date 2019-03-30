767d3a15b5324ab650d83235d957c7ec0943256b
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-03-30 18:23:51 +0100

    test: fix sequelize bug & add root test

--- test/test.js
+++ test/test.js
@@ -7,6 +7,7 @@ const is = require("@slimio/is");
 const argon2 = require("argon2");
 const japa = require("japa");
 const Sequelize = require("sequelize");
+const { get } = require("httpie");
 
 // Require Internal Dependencies
 const models = require("../src/models");
@@ -14,6 +15,7 @@ const httpServer = require("../src/httpServer");
 
 // CONSTANTS
 const HTTP_PORT = 2777;
+const HTTP_URL = new URL(`http://localhost:${HTTP_PORT}`);
 const DB_PATH = join(__dirname, "db_test.db");
 
 // Globals
@@ -27,7 +29,6 @@ japa.group("test", (group) => {
             dialect: "sqlite",
             logging: false
         });
-        sequelize.close();
 
         const tables = models(sequelize);
         await sequelize.sync({ force: true });
@@ -68,8 +69,12 @@ japa.group("test", (group) => {
         await unlink(DB_PATH);
     });
 
-    japa("title", (assert) => {
-        assert.equal(1, 1);
+    japa("root endpoint must return uptime as JSON", async(assert) => {
+        const { data, statusCode } = await get(HTTP_URL);
+        assert.equal(statusCode, 200, "GET Request must return code 200");
+        assert.equal(is.plainObject(data), true, "Returned data must be a plain Object");
+        assert.deepEqual(Object.keys(data), ["uptime"], "Returned data must only have 'uptime' field");
+        assert.equal(is.number(data.uptime), true, "uptime must be typeof number");
     });
 });
 
