d1c05c941d4128b21b3d00fada66b08a0190ba40
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-03-31 22:52:36 +0200

    refactor: improve orgs routes & comment create endpoint

--- src/routes/organisation.js
+++ src/routes/organisation.js
@@ -8,25 +8,27 @@ const { isAuthenticated } = require("../utils.js");
 // Create Router
 const server = polka();
 
-// all oraganisations endpoint
+// all organisations endpoint
 server.get("/", async(req, res) => {
     try {
         const organisations = await req.Organisation.findAll({
-            attributes: { exclude: ["id", "ownerId"] },
-            include: [{
-                model: req.Users,
-                as: "owner",
-                attributes: { exclude: ["id", "password"] }
-            },
-            {
-                model: req.Users,
-                attributes: { exclude: ["id", "password"] },
-                through: { attributes: [] }
-            },
-            {
-                model: req.Addons,
-                attributes: { exclude: ["id", "authorId", "organisationId"] }
-            }]
+            attributes: ["name", "description"],
+            include: [
+                {
+                    model: req.Users,
+                    as: "owner",
+                    attributes: { exclude: ["id", "password", "createdAt", "updatedAt"] }
+                },
+                {
+                    model: req.Users,
+                    attributes: { exclude: ["id", "password"] },
+                    through: { attributes: [] }
+                },
+                {
+                    model: req.Addons,
+                    attributes: { exclude: ["id", "authorId", "organisationId"] }
+                }
+            ]
         });
 
         return send(res, 200, organisations);
@@ -36,7 +38,7 @@ server.get("/", async(req, res) => {
     }
 });
 
-// get oraganisations by name endpoint
+// get organisation by name endpoint
 server.get("/:name", async(req, res) => {
     const name = req.params.name;
 
@@ -44,20 +46,22 @@ server.get("/:name", async(req, res) => {
         const organisations = await req.Organisation.findAll({
             attributes: { exclude: ["id", "ownerId"] },
             where: { name },
-            include: [{
-                model: req.Users,
-                as: "owner",
-                attributes: { exclude: ["id", "password"] }
-            },
-            {
-                model: req.Users,
-                attributes: { exclude: ["id", "password"] },
-                through: { attributes: [] }
-            },
-            {
-                model: req.Addons,
-                attributes: { exclude: ["id", "authorId", "organisationId"] }
-            }]
+            include: [
+                {
+                    model: req.Users,
+                    as: "owner",
+                    attributes: { exclude: ["id", "password"] }
+                },
+                {
+                    model: req.Users,
+                    attributes: { exclude: ["id", "password"] },
+                    through: { attributes: [] }
+                },
+                {
+                    model: req.Addons,
+                    attributes: { exclude: ["id", "authorId", "organisationId"] }
+                }
+            ]
         });
 
         if (organisations.length === 0) {
@@ -65,34 +69,6 @@ server.get("/:name", async(req, res) => {
         }
 
         return send(res, 200, organisations[0]);
-    }
-    catch (error) {
-        return send(res, 500, error);
-    }
-});
-
-// create an oraganisation
-server.post("/", isAuthenticated, async(req, res) => {
-    const { name, description } = req.body;
-    const ownerId = req.user.id;
-
-    try {
-        const orgaExist = await req.Organisation.findOne({ where: { name } });
-        if (orgaExist !== null) {
-            return send(res, 500, { error: `Organisation ${name} already exist` });
-        }
-
-        const organisation = await req.Organisation.create({
-            name, description, ownerId
-        });
-
-        const user = await req.Users.findOne({ where: { id: ownerId } });
-        const userIsInCurrentOrg = await organisation.hasUsers(user);
-        if (!userIsInCurrentOrg) {
-            await organisation.addUsers(user);
-        }
-
-        return send(res, 201, { organisationId: organisation.id });
     }
     catch (error) {
         return send(res, 500, error);
@@ -130,30 +106,32 @@ server.post("/:orgaName/:userName", isAuthenticated, async(req, res) => {
     }
 });
 
-// for V2 ?
-// delete oraganisation by name
-// server.delete("/:orgaName", isAuthenticated, async(req, res) => {
-//     const orgaName = req.params.orgaName;
+// create an organisation
+// server.post("/", isAuthenticated, async(req, res) => {
+//     const { name, description } = req.body;
+//     const ownerId = req.user.id;
 
-//     let result;
 //     try {
-//         const organisation = await req.Organisation.findOne({ where: { name: orgaName } });
-//         if (organisation === null) {
-//             return send(res, 500, { error: `Organisation ${orgaName} not found` });
-//         }
-//         if (organisation.ownerId !== req.user.id) {
-//             return send(res, 500, { error: "You have no right on this organisation" });
+//         const orgaExist = await req.Organisation.findOne({ where: { name } });
+//         if (orgaExist !== null) {
+//             return send(res, 500, { error: `Organisation ${name} already exist` });
 //         }
 
-//         result = await req.Organisation.destroy({
-//             where: { name: orgaName }
+//         const organisation = await req.Organisation.create({
+//             name, description, ownerId
 //         });
+
+//         const user = await req.Users.findOne({ where: { id: ownerId } });
+//         const userIsInCurrentOrg = await organisation.hasUsers(user);
+//         if (!userIsInCurrentOrg) {
+//             await organisation.addUsers(user);
+//         }
+
+//         return send(res, 201, { organisationId: organisation.id });
 //     }
 //     catch (error) {
 //         return send(res, 500, error);
 //     }
-
-//     return send(res, 200, { result });
 // });
 
 module.exports = server;
