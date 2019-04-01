469c278d32c6597d85bff4b6b352fbc311230655
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-04-01 10:59:43 +0200

    feat: extend indicative to add SemVer validation

--- src/validationRules.js
+++ src/validationRules.js
@@ -1,3 +1,20 @@
+// Require Third-party Dependencies
+const { validations } = require("indicative");
+const semver = require("semver");
+
+// eslint-disable-next-line
+validations.semver = function (data, field, message, args, get) {
+    return new Promise((resolve, reject) => {
+        const fieldValue = get(data, field);
+
+        if (fieldValue && semver.valid(fieldValue) === null) {
+            return reject(message);
+        }
+
+        return resolve("validation passed");
+    });
+};
+
 const user = {
     username: "required|string|max:40",
     password: "required|string|min:6"
@@ -11,7 +28,7 @@ const publish = {
     name: "required|string|min:2|max:35",
     description: "string|max:120",
     git: "required|string|max:120",
-    version: "required|string"
+    version: "required|string|semver"
 };
 
 module.exports = {
